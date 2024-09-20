4ebb25d139b67085e8c39fc0e9b05e0321fe7506
Tristan Poland (Trident_For_U) <34868944+tristanpoland@users.noreply.github.com>
2024-09-20 12:31:43 -0400

    Update repipe-debug.pl

--- repipe-debug.pl
+++ repipe-debug.pl
@@ -5,51 +5,17 @@ use warnings;
 use File::Basename;
 use File::Temp qw/ tempfile /;
 use Cwd qw/ abs_path /;
-use IPC::Open3;
-use Symbol qw( gensym );
-use IO::Select;
 
-# Improved function to escape special characters for regex
-sub escape_for_regex {
-    my ($string) = @_;
-    $string =~ s/([\/\\\[\]^$.*+?(){}|])/\\$1/g;
-    $string =~ s/\s+/\\s+/g;  # Replace whitespace with \s+
-    $string =~ s/'/\\'/g;     # Escape single quotes
-    return quotemeta($string);  # Use quotemeta for additional escaping
-}
+# spruce-merge-diff-file-blame
+#
+# Script for merging YAML files using Spruce, resolving dynamic placeholders,
+# and tracking file-level blame based on YAML hierarchy.
 
-# Function to safely execute shell commands
-sub safe_execute {
-    my ($cmd) = @_;
-    my ($in, $out, $err);
-    $err = gensym;
-    my $pid = open3($in, $out, $err, $cmd);
-    
-    my $sel = IO::Select->new;
-    $sel->add($out, $err);
-    
-    my $stdout = '';
-    my $stderr = '';
-    
-    while (my @ready = $sel->can_read) {
-        foreach my $fh (@ready) {
-            my $line = <$fh>;
-            unless (defined $line) {
-                $sel->remove($fh);
-                next;
-            }
-            if ($fh == $out) {
-                $stdout .= $line;
-            } else {
-                $stderr .= $line;
-            }
-        }
-    }
-    
-    waitpid($pid, 0);
-    my $exit_status = $? >> 8;
-    
-    return ($stdout, $exit_status, $stderr);
+# Function to escape special characters for grep
+sub escape_for_grep {
+    my ($str) = @_;
+    $str =~ s/([\[\]^$.*()+?{}\|\\])/\\$1/g;
+    return $str;
 }
 
 # Function to resolve params from environment or other sources
@@ -58,28 +24,26 @@ sub resolve_placeholders {
     my $resolved_line = $line;
 
     # Resolve (( param "..." )) with environment variables or default values
-    while ($resolved_line =~ /\(\(\s*param\s+"([^"]+)"\s+\)\)/) {
+    if ($line =~ /\(\(.*param\s+"(.*)"\s+\)\)/) {
         my $param_name = $1;
-        my $param_value = $ENV{$param_name} || "UNDEFINED_PARAM_$param_name";
+        my $param_value = exists $ENV{$param_name} ? $ENV{$param_name} : "UNDEFINED_PARAM_$param_name";
         $resolved_line =~ s/\(\(\s*param\s+"$param_name"\s+\)\)/$param_value/g;
     }
 
     # Resolve (( grab <something> )) by looking it up from the merged YAML or other files
-    while ($resolved_line =~ /\(\(\s*grab\s+([^\s]+)\s+\)\)/) {
+    if ($line =~ /\(\(.*grab\s+(.*)\s+\)\)/) {
         my $grab_target = $1;
-        my ($grab_value, $exit_status, $error) = safe_execute("grep -oP '$grab_target:\\s*\\K.*' merged_temp.yml");
-        $grab_value = "UNDEFINED_GRAB_$grab_target" if $exit_status != 0;
+        my $grab_value = `grep -oP '$grab_target:\\s*\\K.*' merged_temp.yml 2>/dev/null` || "UNDEFINED_GRAB_$grab_target";
         chomp $grab_value;
         $resolved_line =~ s/\(\(\s*grab\s+$grab_target\s+\)\)/$grab_value/g;
     }
 
     # Resolve (( concat ... )) by concatenating the values
-    while ($resolved_line =~ /\(\(\s*concat\s+(.*?)\s+\)\)/) {
+    if ($line =~ /\(\(.*concat\s+(.*)\s+\)\)/) {
         my $concat_targets = $1;
         my $concat_values = "";
         for my $target (split /\s+/, $concat_targets) {
-            my ($value, $exit_status, $error) = safe_execute("grep -oP '$target:\\s*\\K.*' merged_temp.yml");
-            $value = $target if $exit_status != 0;
+            my $value = `grep -oP '$target:\\s*\\K.*' merged_temp.yml 2>/dev/null` || $target;
             chomp $value;
             $concat_values .= $value;
         }
@@ -108,11 +72,11 @@ sub find_hierarchical_line_number {
                 $found_key = 0;
             }
 
-            if ($content =~ /^\Q$key\E:/) {
+            if ($content =~ /^$key:/) {
                 $found_key = 1;
                 $current_indent = $indent_level;
                 $found_line = $line_number;
-            } elsif ($found_key && $content =~ /\Q$value\E/) {
+            } elsif ($found_key && $content =~ /$value/) {
                 close $fh;
                 return $found_line;
             }
@@ -122,26 +86,22 @@ sub find_hierarchical_line_number {
     return $found_line;
 }
 
-# Updated function to merge YAML files using Spruce and track blame with hierarchical approach
+# Function to merge YAML files using Spruce and track blame with hierarchical approach
 sub spruce_merge_with_blame {
     my ($output_file, @files) = @_;
 
     my ($temp_merged_fh, $temp_merged) = tempfile();
 
     print "Performing Spruce merge...\n";
-    my ($spruce_output, $spruce_exit, $spruce_error) = safe_execute("spruce merge @files");
-    die "Spruce merge failed: $spruce_error" if $spruce_exit != 0;
-    print $temp_merged_fh $spruce_output;
-    close $temp_merged_fh;
+    system("spruce merge @files > $temp_merged");
 
-    print "Resolving placeholders and adding blame...\n";
+    print "Resolving placeholders...\n";
     open my $out_fh, '>', $output_file or die "Cannot open file '$output_file': $!";
     print $out_fh "# Debug: Input files: @files\n";
 
     open my $in_fh, '<', $temp_merged or die "Cannot open file '$temp_merged': $!";
     while (my $line = <$in_fh>) {
         chomp $line;
-        
         # Skip comments and empty lines
         if ($line =~ /^#/ || $line =~ /^\s*$/) {
             print $out_fh "$line\n";
@@ -158,25 +118,26 @@ sub spruce_merge_with_blame {
             if ($content =~ /^([a-zA-Z0-9_]+):(.*)$/) {
                 my $key = $1;
                 my $value = $2;
-                $value =~ s/^\s+|\s+$//g;  # Trim whitespace
-                my $escaped_key = escape_for_regex($key);
-                my $escaped_value = escape_for_regex($value);
                 my $found = 0;
                 for my $file (@files) {
-                    my $grep_command = qq(grep -nP '^\\s*$escaped_key:\\s*$escaped_value' "$file");
-                    my ($grep_result, $grep_exit, $grep_error) = safe_execute($grep_command);
-                    if ($grep_exit == 0) {
-                        chomp $grep_result;
-                        my ($line_number) = split /:/, $grep_result;
-                        print $out_fh "${indent}# File: $file (Line: $line_number)\n";
-                        print $out_fh "$resolved_line\n";
-                        $found = 1;
-                        last;
+                    open my $file_fh, '<', $file or die "Cannot open file '$file': $!";
+                    while (my $file_line = <$file_fh>) {
+                        if ($file_line =~ /^\s*\Q$key\E:\s*\Q$value\E/) {
+                            my $line_number = find_hierarchical_line_number($file, $key, $value);
+                            if ($line_number) {
+                                print $out_fh "${indent}# File: $file (Line: $line_number)\n";
+                                print $out_fh "$resolved_line\n";
+                                $found = 1;
+                                last;
+                            }
+                        }
                     }
+                    close $file_fh;
+                    last if $found;
                 }
                 if (!$found) {
                     print $out_fh "$resolved_line\n";
-                    warn "Warning: Key-value pair '$key: $value' not found in any file\n";
+                    warn "Warning: Key-value pair '$key:$value' not found in any file\n";
                 }
             } else {
                 print $out_fh "$resolved_line\n";
@@ -188,24 +149,20 @@ sub spruce_merge_with_blame {
     close $in_fh;
     close $out_fh;
 
+    unlink $temp_merged;
+
     print "Final merged file with blame:\n";
     system("cat $output_file");
-
-    unlink $temp_merged;
 }
 
 # Main script
 sub main {
     my $base_dir = dirname(abs_path($0));
-    chdir $base_dir or die "Cannot change to directory '$base_dir': $!";
+    chdir $base_dir or die "Cannot change directory to $base_dir: $!";
 
     # Prepare input files
-    my @input_files = (
-        'pipeline/base.yml',
-        glob('pipeline/*/*.yml'),
-        'settings.yml'
-    );
-    @input_files = grep { !/pipeline\/custom.*\/.*\.yml/ && !/pipeline\/optional.*\/.*\.yml/ } @input_files;
+    my @input_files = grep { !/pipeline\/custom.*\/.*\.yml/ && !/pipeline\/optional.*\/.*\.yml/ } 
+                      glob("pipeline/base.yml pipeline/*/*.yml settings.yml");
 
     if (@ARGV < 1) {
         die "Usage: $0 <output_merged.yml>\n";
